2916723894619d8d3e323b9001ec2fde71aba8bc
Yashu200207 <hsyashu4@gmail.com>
2024-08-06 19:04:53 +0530

    Initial commit of Next.js project with Tailwind CSS and Mantine

---
+++ app/components/Header.js
@@ -0,0 +1,6 @@
+function Header() {
+    return <header>This is the Header</header>;
+  }
+  
+  export default Header;
+  
---
+++ app/components/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to the Next.js project with Tailwind CSS and Mantine!</p>
+    </div>
+  );
+}
